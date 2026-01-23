8b96f68083b0126110e255eac397c7f606748f3d
LsYueh <lsyueh@gmail.com>
2026-01-24 07:03:39 +0800

    Recursive Descent Parser

--- GetThePicture/Copybook/Compiler/Parser.cs
+++ GetThePicture/Copybook/Compiler/Parser.cs
@@ -3,6 +3,11 @@ using GetThePicture.Copybook.Compiler.Ir;
 
 namespace GetThePicture.Copybook.Compiler;
 
+public sealed record DataItemHeader(
+    int Level,
+    string Name
+);
+
 public class Parser(List<Token> tokens)
 {
     private readonly List<Token> tokens = tokens;
@@ -12,15 +17,26 @@ public class Parser(List<Token> tokens)
     // Pointer
     // ----------------------------
 
+    private Token? _previous;
+
+    public Token? Previous => _previous;
+
     private Token? Current => position < tokens.Count ? tokens[position] : null;
-    
+
     private Token? Lookahead(int n = 1) => (position + n) < tokens.Count ? tokens[position + n] : null;
     
     // ----------------------------
     // Operations
     // ----------------------------
 
-    private Token Consume() => tokens[position++];
+    private Token Consume()
+    {
+        var current = Current ?? throw new InvalidOperationException("Cannot consume EOF.");
+
+        _previous = current;
+
+        return tokens[position++];
+    }
 
     private Token Expect(TokenType type)
     {
@@ -41,61 +57,92 @@ public class Parser(List<Token> tokens)
     /// </summary>
     public IDataItem? Analyze()
     {
-        IDataItem? root = null;
-        
-        while (Current != null)
-        {
-            var level = int.Parse(Expect(TokenType.LevelNumber).Value);
-            var name  = Expect(TokenType.AlphanumericLiteral).Value;
-
-            switch (Current.Type)
-            {
-                case TokenType.Dot:
-                {
-                    root = new GroupItem(level, name);
-                    break;
-                }
-                case TokenType.Occurs:
-                {
-                    Consume(); // OCCURS
-                    int occurs = int.Parse(Expect(TokenType.NumericLiteral).Value);
-
-                    root = new GroupItem(level, name, occurs);
-                    break;
-                }
-                case TokenType.Picture:
-                {
-                    string picString = "";
-
-                    // TODO: Pic Grammar Parser
-
-                    Consume(); // PIC
-                    picString += Expect(TokenType.Identifier).Value;
-                    picString += Expect(TokenType.LParen).Value;
-                    picString += Expect(TokenType.NumericLiteral).Value;
-                    picString += Expect(TokenType.RParen).Value;
-
-                    var pic = Pic.Parse(picString);
-                    root = new ElementaryDataItem(level, name, pic);
-                    break;
-                }
-                default:
-                    throw new CompileException(
-                        $"Invalid or unsupported clause after data item '{name}'. " +
-                        $"Expected OCCURS or PIC.",
-                        Current
-                    );
-            }
-
-            Expect(TokenType.Dot);
-        }
-
-        return root;
+        return ParseDataItem();
     }
 
     // ----------------------------
     // Grammar
     // ----------------------------
 
+    /// <summary>
+    /// Recursive Descent Parser (Top-down Parser).
+    /// </summary>
+    /// <param name="parent"></param>
+    /// <returns></returns>
+    /// <exception cref="CompileException"></exception>
+    private IDataItem? ParseDataItem(GroupItem? parent = null)
+    {
+        // 遞迴終止
+        if (Current == null) return null;
+
+        // 解析
+        IDataItem subordinate = ParseSingleDataItem();
+
+        parent?.AddSubordinate(subordinate);
+
+        // 解析子項目（只有 GroupItem 才能有 subordinate）
+        if (subordinate is GroupItem group)
+        {
+            while (Current != null && Current.Type == TokenType.LevelNumber)
+            {
+                int nextLevel = int.Parse(Current.Value);
+
+                if (nextLevel <= group.Level)
+                    break;
+
+                ParseDataItem(group);
+            }
+        }
+
+        return subordinate;
+    }
+
+    private (int Level, string Name) ParseDataItemHeader()
+    {
+        var level = int.Parse(Expect(TokenType.LevelNumber).Value);
+        var name  = Expect(TokenType.AlphanumericLiteral).Value;
+
+        return (level, name);
+    }
+
+    private IDataItem ParseSingleDataItem()
+    {
+        var (level, name) = ParseDataItemHeader();
+
+        IDataItem item;
+
+        switch (Current?.Type)
+        {
+            case TokenType.Dot:
+                item = new GroupItem(level, name);
+                break;
+
+            case TokenType.Occurs:
+                Consume(); // OCCURS
+                int occurs = int.Parse(Expect(TokenType.NumericLiteral).Value);
+                item = new GroupItem(level, name, occurs);
+                break;
+
+            case TokenType.Picture:
+                Consume(); // PIC
+
+                string picString = "";
+                picString += Expect(TokenType.Identifier).Value;
+                picString += Expect(TokenType.LParen).Value;
+                picString += Expect(TokenType.NumericLiteral).Value;
+                picString += Expect(TokenType.RParen).Value;
+
+                var pic = Pic.Parse(picString);
+                item = new ElementaryDataItem(level, name, pic);
+                break;
+
+            default:
+                throw new CompileException($"Invalid or unsupported clause after data item '{name}'.", Current ?? Previous);
+        }
+
+        Expect(TokenType.Dot);
+
+        return item;
+    }
 
 }
