e48479796b77b225f086d42c3b21732cdbac3506
LsYueh <lsyueh@gmail.com>
2026-04-22 06:53:19 +0800

    Refactor namespace for CbResolverTest and add CbWrapperTest with initial tests

--- GetThePicture.Tests/Copybook/Resolver/CbResolverTest.cs
+++ GetThePicture.Tests/Copybook/Resolver/CbResolverTest.cs
@@ -7,7 +7,7 @@ using GetThePicture.Copybook.Resolver.Storage;
 using GetThePicture.Picture.Clause.Utils;
 using GetThePicture.TestData;
 
-namespace GetThePicture.Tests.Copybook.Compiler;
+namespace GetThePicture.Tests.Copybook.Resolver;
 
 [TestClass]
 public class CbResolverTest
